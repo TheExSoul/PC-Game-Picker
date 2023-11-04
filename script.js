dd49f2fd40b0e004624ee59325d6100a9c50adf2
michael garcia <michael_Garcia201211@yahoo.com>
2023-11-04 18:30:54 -0400

    FixingAPI

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 
-console.log('hello')
+
+export function handleClick() {
+    console.log("hello")
+}
+
+// console.log('hello')
 
 async function fetchGames() {
-const result = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=76561198168567037&format=json&include_appinfo=true`, 
+const result = fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=ECC9AA477EA7038A7632378B2507747D&steamid=76561198168567037&format=json&include_appinfo=true`, 
 {method: "GET"});
     
     console.log(result);
 }
 
-fetchGames();
+ fetchGames();
+
+// var steamid = document.getElementById()
+
 
