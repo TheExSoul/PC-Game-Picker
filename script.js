51762d9531fc616d4cd92094029818acb87ca0d0
michael garcia <michael_Garcia201211@yahoo.com>
2023-11-04 18:40:20 -0400

    qick fix

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ export function handleClick() {
 // console.log('hello')
 
 async function fetchGames() {
-const result = fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=ECC9AA477EA7038A7632378B2507747D&steamid=76561198168567037&format=json&include_appinfo=true`, 
+const result = fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=ECC9AA477EA7038A7632378B2507747D&steamid=76561198168567037&format=json&include_appinfo=true`, 
 {method: "GET"});
     
     console.log(result);
