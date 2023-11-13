8f1f5c6226da655e438a19900f847a53c9e2e48d
michael garcia <michael_Garcia201211@yahoo.com>
2023-11-12 20:57:54 -0500

    more API changes

--- script.js
+++ script.js
@@ -10,54 +10,82 @@ async function fetchGenres() {
     let genresDropdown = document.getElementById('genres');
     let genresResponseObject = await genresResponse.json();
     let options = genresResponseObject.results;
-    for(let option of options){
-  let optionToAdd = document.createElement('option');
-      optionToAdd.innerHTML = option.name;
-      optionToAdd.value = option.id
-      genresDropdown.appendChild(optionToAdd);
-  }
-  }
-   fetchGenres();
+    for (let option of options) {
+        let optionToAdd = document.createElement('option');
+        optionToAdd.innerHTML = option.name;
+        optionToAdd.value = option.id
+        genresDropdown.appendChild(optionToAdd);
+    }
+}
+fetchGenres();
 
 // calling RAWG API to display list if storefronts in a dropdown menu
-   async function fetchStores() {
+async function fetchStores() {
     const storesResponse = await fetch(`https://api.rawg.io/api/stores?key=${apiKey}`);
     let storesDropdown = document.getElementById('stores');
     let storesResponseObject = await storesResponse.json();
     let options = storesResponseObject.results;
-    for(let option of options){
-  let optionToAdd = document.createElement('option');
-      optionToAdd.innerHTML = option.name;
-      optionToAdd.value = option.id
-      storesDropdown.appendChild(optionToAdd);
-  }
-  }
-   fetchStores();
+    let excludedStores = [9, 11]
+    for (let option of options) {
+        if (!excludedStores.includes(option.id) ) {
+            let optionToAdd = document.createElement('option');
+            optionToAdd.innerHTML = option.name;
+            optionToAdd.value = option.id
+            storesDropdown.appendChild(optionToAdd);
+        }
+    }
+}
+fetchStores();
 
 // calling RAWG API to display list of platforms in a dropdown menu
 
-    async function fetchPlatform() {
+async function fetchPlatform() {
     const platformsResponse = await fetch(`https://api.rawg.io/api/platforms?key=${apiKey}`);
     let platformsDropdown = document.getElementById('platforms');
     let platformsResponseObject = await platformsResponse.json();
     let options = platformsResponseObject.results;
-    for(let option of options){
-  let optionToAdd = document.createElement('option');
-      optionToAdd.innerHTML = option.name;
-      optionToAdd.value = option.id
-      platformsDropdown.appendChild(optionToAdd);
-  }
-  }
-   fetchPlatform();
+    for (let option of options) {
+        let optionToAdd = document.createElement('option');
+        optionToAdd.innerHTML = option.name;
+        optionToAdd.value = option.id
+        platformsDropdown.appendChild(optionToAdd);
+    }
+}
+fetchPlatform();
 // building the api based on inputs from user
-   function buildApiUrl(){
-   let genres = document.getElementById('genres').value;
-   let stores = document.getElementById('stores').value;
-   let platforms = document.getElementById('platforms').value;
+async function fetchGames() {
+    let genres = document.getElementById('genres').value;
+    let stores = document.getElementById('stores').value;
+    let platforms = document.getElementById('platforms').value;
+    let metacritic = document.getElementById('metacritic').value;
+    let apiUrl = `https://api.rawg.io/api/games?key=${apiKey}&page_size=50`
+    if (genres){
+        apiUrl = apiUrl+`&genres=${genres}`   
+    }
+    if (platforms){
+        apiUrl = apiUrl+`&platforms=${platforms}`
+        
+    }
+    if (stores){
+        apiUrl = apiUrl+`&stores=${stores}`
+        
+    }
+    if (metacritic){
+        apiUrl = apiUrl+`&metacritic=${metacritic},100`
+        
+    }
+     console.log(apiUrl)
 
-   let apiUrl = `https://api.rawg.io/api/${genres}/${stores}/${platforms}?key=${apiKey}`
-   console.log(apiUrl)
-   }
+    const searchGamesResponse = await fetch(apiUrl);
 
-   document.getElementById('generateUrlButton').addEventListener('click', buildApiUrl)
+    let searchGamesObject = await searchGamesResponse.json();
+    let gamesList = searchGamesObject.results;
+    let randomNumber = Math.floor(Math.random() * gamesList.length);
+    let randomGame = gamesList[randomNumber]
+    console.log(randomGame)
+
+    
+}
+ 
+document.getElementById('generateUrlButton').addEventListener('click', fetchGames)
 
